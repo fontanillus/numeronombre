e516cf24eff8bb26149b126c1cbb1b43993bfac9
fontanillus <arcamapa@gmail.com>
2025-06-26 12:25:09 +0200

    Add files via upload

---
+++ js/nombre.js
@@ -0,0 +1,13 @@
+const numerosNombre = [
+      { numero: 1, personalidad: 'Pionero e Innovador' },
+      { numero: 2, personalidad: 'Solidario y buen gestor' },
+      { numero: 3, personalidad: 'Consejero y motivador' },
+      { numero: 4, personalidad: 'Disciplinado y ejecutor' },
+      { numero: 5, personalidad: 'Espíritu libre' },
+      { numero: 6, personalidad: 'Protector y responsable' },
+      { numero: 7, personalidad: 'Especialista y perfeccionista' },
+      { numero: 8, personalidad: 'Gran estratega' },
+      { numero: 9, personalidad: 'Genio creativo e idealista' },
+      { numero: 11, personalidad: 'Maestro transformador' },
+      { numero: 22, personalidad: 'El poder puesto al servicio del espíritu' }
+    ];
